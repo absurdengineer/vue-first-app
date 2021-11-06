8b86926a11d03de62569fca5d71b42035c258ac8
Md. Dilshad Alam <mddalam1@gmail.com>
2021-11-06 09:49:54 +0530

    CSS transitions with Vue.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,18 @@
 <template>
   <h3>Home Page</h3>
+  <button @click="flag = !flag">Toggle</button>
+  <transition name="fade" appear mode="out-in">
+    <h5 v-if="flag" key="main">This is First Text</h5>
+    <h5 v-else key="secondary">This is Another Text</h5>
+  </transition>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return { flag: true };
+  },
   unmounted() {
     console.log("Home Component Unmounted.");
   },
@@ -17,4 +25,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
